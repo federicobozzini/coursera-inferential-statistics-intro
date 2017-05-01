60f1a801d544cd88c38aa8dc3f8ac458b282222b
iit-federico-bozzini <federico.bozzini@iit.it>
2017-05-01 12:42:13 +0200

    Added all material.

---
+++ lab0/lab0.R
@@ -0,0 +1,33 @@
+lab0 <-function() {
+        
+        #setup
+        source("http://www.openintro.org/stat/data/present.R")
+        setwd("~/software/statistics/lab0")
+        
+        
+        #ex1
+        res1 <- length(present)
+        cat('res 1: ', res1, '\n')
+        
+        #ex2
+        res2 <- "present$girl"
+        cat('res 2: ', res2, '\n')
+        
+        #ex3
+        cat('res 3:  graph', '\n')        
+        plot(x = present$year, y = present$girls)
+        
+        #ex4
+        res4 <- present[which.max(present$boys + present$girls), 'year']
+        cat('res 4: ', res4, '\n')    
+        
+        #ex5
+        res5 <- all(present$boys > present$girls)
+        cat('res 5 (boys > girls): ', res5, '\n')  
+        
+        #ex8
+        res8 <- present[which.max(abs(present$boys - present$girls)), 'year']
+        cat('res 8: ', res8, '\n')   
+        
+        
+}
